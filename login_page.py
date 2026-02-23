6d239d41b4661e1f54020b5dca39c153d59d15f7
NineTeen1199 <teen423mo@gmail.com>
2026-02-23 13:56:12 +0700

    Add files via upload

--- login_page.py
+++ login_page.py
@@ -2,42 +2,21 @@
 import streamlit as st
 import controller
 
-def render_login():
-    st.title("🔐 เข้าสู่ระบบ")
-
-    with st.form("login_form"):
-        username = st.text_input("ชื่อผู้ใช้", placeholder="เช่น admin")
-        password = st.text_input("รหัสผ่าน", type="password", placeholder="เช่น 1234")
-        submitted = st.form_submit_button("Login")
-
-    if submitted:
-        ok, msgs, user_info = controller.login(username, password)
-        if not ok:
-            for m in msgs:
-                st.error(m)
-        else:
-            for m in msgs:
-                st.success(m)
-            
-            st.session_state["is_logged_in"] = True
-            # --- ส่วนต่อเนื่องจากอีกรูป ---
-            st.session_state["user"] = user_info
-            st.session_state["page"] = "books" # หรือให้ไป borrows ก็ได้
-            st.rerun()
-
-
-
-            ###########################
-
-
-            # pages/login_page.py
-
-import streamlit as st
-import controller
-
 
 def render_login():
     st.title("🔐 เข้าสู่ระบบ")
+
+    # ===== เพิ่มข้อมูลผู้จัดทำ =====
+    st.markdown("""
+    <div style=' margin-bottom:20px;'>
+        <h4>ข้อมูลผู้จัดทำ</h4>
+        <p style='font-size:16px; font-weight:bold;'>
+            นาย อภิวัฒน์ พุ่มแดง<br>
+            6740259122<br>
+            ว.6706
+        </p>
+    </div>
+    """, unsafe_allow_html=True)
 
     with st.form("login_form"):
         username = st.text_input(
@@ -63,5 +42,5 @@ def render_login():
 
             st.session_state["is_logged_in"] = True
             st.session_state["user"] = user_info
-            st.session_state["page"] = "books"   # หรือเปลี่ยนเป็น "borrows"
+            st.session_state["page"] = "books"
             st.rerun()
